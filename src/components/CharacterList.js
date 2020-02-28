24e72b557f6d3ca83d5d3c863aa784784268a95d
Sam Decker <samdecker386@gmail.com>
2020-02-28 11:45:26 -0700

    Coompleted MVP

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import SearchForm from './SearchForm';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+      .then(response => setCharacters(response.data.results))
+      .catch(error => console.log(error));
+    console.log();
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <div>
+      <SearchForm characters={characters} />
+    </div>
   );
 }
